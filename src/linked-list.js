e0481a7743c0fe5c8278fa4fc8721d0f340d6e9c
molestador <hustlerxxl@gmail.com>
2017-01-30 15:30:02 +0200

    doubly-linked-list/feature: implement few methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,105 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(data);
+        if (this.length === 0) {
+            this._head = node;
+            this._tail = node;
+        } else {
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+        }
+        this.length++;
+        return;
+    }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+        let current = this._head,
+            length = this.length,
+            i = 0;
+        if (length === 0 || index < 0 || index > length) {
+            throw new Error('not exist');
+        }
+        while (i < index) {
+            current = current.next;
+            i++;
+        }
+        return current.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        var node = new Node(data);
+        var temp = this._head;
+        var i = 0;
+        while (i !== this.length) {
+            if (i === index) {
+                node.prev = temp.prev;
+                node.next = temp;
+                temp.prev.next = node;
+                return;
+            }
+            temp = temp.next;
+            i++;
+        }
+    }
 
-    isEmpty() {}
+    isEmpty() {
+        if (this.length === 0) {
+            return true;
+        }
+        else return false;
+    }
 
-    clear() {}
+    clear() {
 
-    deleteAt(index) {}
+    }
 
-    reverse() {}
+    deleteAt(index) {
+        let temp = this._head;
+        let prev = null;
+        if (index === 0) {
+            temp = temp.next;
+            return;
+        }
+        for (let i = 1; i <= index && temp; i += 1) {
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = temp.next;
+    };
 
-    indexOf(data) {}
+    reverse() {
+
+    }
+
+    indexOf(data) {
+        var node = this._head;
+        var i = 0;
+        while (i !== this.length) {
+            if (node.data === data) {
+                return i;
+            }
+            node = node.next;
+            i++;
+        }
+        return i = -1;
+    }
 }
+
+
 
 module.exports = LinkedList;
